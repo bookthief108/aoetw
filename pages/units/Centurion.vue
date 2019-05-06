7fe7a16fc1f275511c4cd4302ae4d744b9820c3e
bookthief108 <47220722+bookthief108@users.noreply.github.com>
2019-05-06 20:41:59 +0800

    Create Centurion.vue

---
+++ pages/units/Centurion.vue
@@ -0,0 +1,132 @@
+<template>
+  <!-- Main jumbotron for a primary marketing message or call to action
+    -->
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row padtop">
+      <div class="col-md-12">
+        <h1>百夫長</h1>
+        <div class="hrn1"></div>
+      </div>
+      <div class="col-md-8">
+        <div class="n1">
+          <p>百夫長首次在《世紀帝國II:失落的帝國》登場。百夫長是一個不能被訓練的單位，只在地圖編輯器
+          跟阿拉里克"條條大路通圍城"以及巴里"邁勒斯叛變"的戰役中出現。百夫長的外表與聖騎兵幾乎一樣，而
+          它的狀態與精銳聖騎兵相去不遠，只有長相稍微不同而已。</p>
+          
+        </div>
+      </div>
+      <div class="col-md-4">
+        <table class="table-bordered" width="100%">
+          <tr>
+            <td
+              class="unit"
+              colspan="2"
+              style="background-color:#3c763d;text-align: center;"
+            >
+              <div class="unititle" style="color: #fff">百夫長</div>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div class="unititle">
+                <img src="~assets/Centurionicon.png" width="45" alt="" />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2">單位介紹</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">類型</td>
+            <td class="unit4">
+              <nuxt-link to="/units/Cavalry_Units">騎兵</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">首次出現</td>
+            <td class="unit4">
+            <nuxt-link to="/games/aof">失落的帝國</nuxt-link>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit" colspan="2" style="background-color:#3c763d;;">
+              <div class="unititle2" style="color: #fff;">數據</div>
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">血量</td>
+            <td class="unit4">150</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊</td>
+            <td class="unit4">12(近戰)</td>
+          </tr>
+          <tr>
+            <td class="unit3">攻擊加成</td>
+            <td class="unit4">
+              +12對<nuxt-link to="/armor/Armor_Class_Infantry"
+                >步兵</nuxt-link>
+              <br />+10對<nuxt-link to="/armor/Armor_Class_Condottiero"
+                >傭兵</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">攻速</td>
+            <td class="unit4">1.73</td>
+          </tr>
+          <tr>
+            <td class="unit3">近防</td>
+            <td class="unit4">2</td>
+          </tr>
+          <tr>
+            <td class="unit3">遠防</td>
+            <td class="unit4">1</td>
+          </tr>
+          <tr>
+            <td class="unit3">防禦類型</td>
+            <td class="unit4">
+              <nuxt-link to="/armor/Armor_Class_Cavalry_Archer">騎兵</nuxt-link
+              >(+16) <br /><nuxt-link
+                to="/armor/Armor_Class_Unique_Unit"
+                >特殊單位</nuxt-link
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="unit3">跑速</td>
+            <td class="unit4">1.35</td>
+          </tr>
+          <tr>
+            <td class="unit3">視野</td>
+            <td class="unit4">5(精銳)</td>
+          </tr>
+        </table>
+      </div>
+      <div class="col-md-12">
+        <h3>圖庫</h3>
+        <div class="hrn1"></div>
+        <div class="n1">
+          <div class="col-md-12">
+            <div class="col-md-4">
+              <nuxt-link to="/img/Cataphract.png"
+                ><img class="imgn hg" src="~assets/Centurion.png" alt=""
+              /></nuxt-link>
+              <p><em>一群百夫長</em></p>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'allunit',
+  components: {}
+}
+</script>
+<style></style>
